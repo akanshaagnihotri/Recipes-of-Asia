b852df00da7c503f6eb1a6ba5fba14169f345750
Akansha Agnihotri <shubhiakansha96@gmail.com>
2021-11-23 12:47:02 -0500

    Google Analytics Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,30 @@ import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
 
 const Noop: FC = ({ children }) => <>{children}</>
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
+  const router = useRouter()
 
   useEffect(() => {
-    document.body.classList?.remove('loading')
-  }, [])
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
